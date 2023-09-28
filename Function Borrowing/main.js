35afaed60aad2b13a6c23e1ea00888645f2833a5
goldic <goldicstudent@gmail.com>
2023-09-28 10:12:23 +0400

    learn

--- Function Borrowing/main.js
+++ Function Borrowing/main.js
@@ -27,13 +27,25 @@ const planet = {
 };
 
 const planet2 = {
-    size: 9423,
-    named: "Jupiter",
-}
+  size: 9423,
+  named: "Jupiter",
+};
 
-planet.sayHi("Alex")
-planet.sayHi.call(planet2,"Bob") // anotyher params, cant do in bind(). call() just called functions :) and also retuns result of it
+planet.sayHi("Alex");
+planet.sayHi.call(planet2, "Bob"); // anotyher params, cant do in bind(). call() just called functions :) and also retuns result of it
 
 //Apply()
 
+const fruit = {
+  name: "apple",
+  piece: function (person, web_site) {
+    return `Hello from ${person} on web-site ${web_site}, ${this.name}`;
+  },
+};
 
+const fruit2 = {
+  name: "orange",
+};
+
+const result = fruit.piece.apply(fruit2, ["Alex", "Youtube"]);
+console.log(result);
