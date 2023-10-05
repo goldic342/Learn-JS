1a54f14fef83d2585e1eca0fc3575a0804f4c112
goldic <goldicstudent@gmail.com>
2023-10-05 08:44:48 +0400

    learn

--- Events basics/index.js
+++
@@ -1,9 +0,0 @@
-//<-->
-const healtPoints = document.getElementById("healt-points");
-healtPoints.innerHTML = 10;
-const damage = function () {
-  if (healtPoints.innerHTML > 0) {
-    healtPoints.innerHTML -= 1;
-  }
-};
-healtPoints.addEventListener("click", damage);
